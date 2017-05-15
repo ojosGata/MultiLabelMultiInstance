a67c70642f50cfaf9ad3641bafc34500b5794d67
ojosGata <reyesmeleroanaisabel@gmail.com>
2017-05-15 12:45:09 +0200

    Delete Transformer.java

--- MIML3/src/mimltransformation/Transformer.java
+++
@@ -1,9 +0,0 @@
-package mimltransformation;
-
-import data.MIMLInstances;
-import mulan.data.MultiLabelInstances;
-
-public interface Transformer {
-	public MultiLabelInstances transformDataset(MIMLInstances dataset);
-	public MultiLabelInstances transformInstance(MIMLInstances dataset);
-}
